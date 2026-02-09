96b75ba6d8e4bc6c05d7fb05eaf94732dbec4d07
Volodya Byelobrov <volodia.byelobrov@gmail>
2026-02-09 22:55:15 +0100

    minimax not finished

--- Chapter05/Stack_based_queue.py
+++ Chapter05/Stack_based_queue.py
@@ -1,31 +1,28 @@
-class Queue:  
-    def __init__(self):  
-        self.Stack1 = []  
-        self.Stack2 = []  
-                
-    def enqueue(self, data):  
-        self.Stack1.append(data)
-        
-        
+class Queue:
+    def __init__(self):
+        self.Stack1 = []
+        self.Stack2 = []
 
-        def dequeue(self):   
-        if not self.Stack2:  
-            while self.Stack1:  
-                self.Stack2.append(self.Stack1.pop())  
+    def enqueue(self, data):
+        self.Stack1.append(data)
+
+        if not self.Stack2:
+            while self.Stack1:
+                self.Stack2.append(self.Stack1.pop())
         if not self.Stack2:
             print("No element to dequeue")
             return
-        return self.Stack2.pop()  
-      
-      
-queue = Queue()  
-queue.enqueue(5)  
-queue.enqueue(6)  
-queue.enqueue(7)  
-print(queue.Stack1)  
+        return self.Stack2.pop()
 
-queue.dequeue()  
-print(queue.Stack1)  
-print(queue.Stack2)  
-queue.dequeue()  
-print(queue.Stack2)        
+
+queue = Queue()
+queue.enqueue(5)
+queue.enqueue(6)
+queue.enqueue(7)
+print(queue.Stack1)
+
+queue.dequeue()
+print(queue.Stack1)
+print(queue.Stack2)
+queue.dequeue()
+print(queue.Stack2)
